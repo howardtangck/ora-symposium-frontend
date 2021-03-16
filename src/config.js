31005d5db82114e4298884af9f036f39747cd9ec
howardtangck <howardtangck@gmail.com>
2021-03-16 20:42:13 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    api_server: 'http://193.123.252.120/',
+    api_server: 'http://193.123.252.120:4000/',
     path_authenticate: 'users/authenticate',
     path_getLatestPosts: 'posts/feed/',
     path_resetPassword: 'users/resetPassword',
@@ -7,6 +7,7 @@ export const config = {
     path_createMessage: 'posts/create',
     path_validate_otp: 'users/validateOTP',
     path_generate_otp: 'users/generateOTP',
-    obj_store_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/p/ShyUyJr14FZAa4rMfwNRvAFgPYzhLWp4IULM_vTv-yQ/n/gse00014894/b/sc_symposium/o/",
-    obj_store_read_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/n/gse00014894/b/sc_symposium/o/",
+    obj_store_url: "https://objectstorage.ap-seoul-1.oraclecloud.com/p/_FMYjnqCFEvrwua7Km4c08JNgLGvwrtjNp_A8lC8UZgZ1KvprVAlPStL1uoNz482/n/cnikxkshlqhn/b/bucket-20210316-1957/o/",
+    obj_store_read_url: "https://objectstorage.ap-seoul-1.oraclecloud.com/p/_FMYjnqCFEvrwua7Km4c08JNgLGvwrtjNp_A8lC8UZgZ1KvprVAlPStL1uoNz482/n/cnikxkshlqhn/b/bucket-20210316-1957/o/",
+   
 }
